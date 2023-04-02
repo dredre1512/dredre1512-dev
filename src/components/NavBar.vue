703a1791a147a7063a4ea3202346482cfa7ba8b1
dredre1512 <129392083+dredre1512@users.noreply.github.com>
2023-04-02 20:33:47 +0800

    Add files via upload

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,18 +2,14 @@
 	<nav class="shadow navbar navbar-expand-lg bg-body-tertiary sticky-top">
 		<div class="container">
 			<a class="navbar-brand" href="#">API</a>
-			<button
-				class="navbar-toggler"
-				type="button"
-				data-bs-toggle="collapse"
-				data-bs-target="#navbarSupportedContent"
-				aria-controls="navbarSupportedContent"
-				aria-expanded="false"
-				aria-label="Toggle navigation"
-			>
+			<button @click="toggle = !toggle" class="navbar-toggler" type="button">
 				<span class="navbar-toggler-icon"></span>
 			</button>
-			<div class="collapse navbar-collapse" id="navbarSupportedContent">
+			<div
+				class="collapse navbar-collapse"
+				:class="{ show: toggle }"
+				id="navbarSupportedContent"
+			>
 				<ul class="navbar-nav me-auto mb-2 mb-lg-0">
 					<li
 						v-for="(link, idx) in links"
@@ -38,13 +34,14 @@
 import { ref, computed } from "vue";
 import { useRoute } from "vue-router";
 const route = useRoute();
-
+const toggle = ref(false);
 const links = ref([
 	{ name: "Home", path: "/" },
 	{ name: "Cats", path: "/cats" },
 	{ name: "Quotes", path: "/quotes" },
 	{ name: "TedTalk", path: "/ted-talk" },
 	{ name: "News", path: "/news" },
+	{ name: "Numbers", path: "/numbers" },
 	{ name: "Countries", path: "/countries" },
 ]);
 const activeLink = computed(() => {
